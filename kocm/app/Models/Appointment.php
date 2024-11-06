3f02fec4c90448e1bb1bdc333e28317f60e3deef
Maisok <miasnikov2005@gmail.com>
2024-11-06 11:02:52 +0800

    meow

---
+++ kocm/app/Models/Appointment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+   /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'phone',
+        'user_id',
+    ];
+
+    /**
+     * Get the user that owns the appointment.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ kocm/database/migrations/2024_11_06_022643_create_new_appointments_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('new_appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
+            $table->foreignId('specialist_id')->constrained('specialists')->onDelete('cascade');
+            $table->foreignId('service_id')->nullable()->constrained('services')->onDelete('set null'); 
+            $table->string('name');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+   
+    public function down(): void
+    {
+        Schema::dropIfExists('new_appointments');
+    }
+};
